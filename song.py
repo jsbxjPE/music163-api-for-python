b92ce94f662031b230cadefac04eec0de1999a10
HHRH on minecraft mods <C3424233784@163.com>
2022-03-11 12:52:31 +0800

    1.0

---
+++ song.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from time import sleep
+
+global so_name
+global so_mp3
+so_name = ''
+so_mp3 = ''
+
+headers = {'Referer': 'http://music.163.com/',
+           'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.36'
+}
+
+def music(song_id):
+    global so_name
+    so_name = ''
+    so_mp3 = (json.loads(requests.get('https://api.muxiaoguo.cn/api/163music?id={}'.format(song_id)).text)['data']['mp3url'])
+    return so_mp3, so_name
+
+def download(path):
+    global so_mp3
+    global so_name
+    content = requests.get(url=so_mp3, headers=headers).content
+    name = '{}/{}.mp3'.format(path, so_name)
+    with open(name, 'wb') as f:
+        f.write(content)
+    #sleep(1.4)
